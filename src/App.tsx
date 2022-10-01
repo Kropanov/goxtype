ab16ab6267de5b313a8024e27e8ad1e9009a7d34
Maxim Kropanov <kropanov2012@gmail.com>
2022-10-01 16:41:53 +0300

    Adding system theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Header from "./components/Header/Header";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
     return (
-        <Header />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header />
+        </ThemeProvider>
     );
 }
 
