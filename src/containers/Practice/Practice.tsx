58b905f2498d2d790495fb0db87502d6be0c36e2
Maxim Kropanov <kropanov2012@gmail.com>
2022-12-06 21:29:56 +0300

    Create Typing Container for practice

--- src/containers/Practice/Practice.tsx
+++ src/containers/Practice/Practice.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import TypingContainer from "../../components/TypingContainer/TypingContainer";
 
 export default function Practice() {
     return (
         <Container maxWidth="md">
             <Grid container spacing={2}>
                 <Grid item lg={12}>
-                    Some text
+                    <TypingContainer />
                 </Grid>
             </Grid>
         </Container>
