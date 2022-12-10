b279d19e3a938abf0af6f6856c3409cce2867bfa
Maxim Kropanov <kropanov2012@gmail.com>
2022-12-11 00:26:02 +0300

    Adding text field state

--- src/components/TypingContainer/InputTextField/InputTextField.tsx
+++ src/components/TypingContainer/InputTextField/InputTextField.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {TextField} from "@mui/material";
 
 export default function InputTextField() {
+
+    const [textFieldValue, setTextFieldValue] = useState("");
+
+    const onChangeTextFieldValue = (event:  React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setTextFieldValue(event.target.value);
+    };
+
+    useEffect(() => {
+        console.log(textFieldValue);
+    }, [textFieldValue]);
+
     return (
         <TextField
             fullWidth
+            value={textFieldValue}
+            onChange={(event) => onChangeTextFieldValue(event)}
             label="Please enter text"
         />
     );
