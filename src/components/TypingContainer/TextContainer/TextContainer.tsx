a61f0eb9390d6f0b2aebf8fa5ddf2d9baa53bf40
Maxim Kropanov <kropanov2012@gmail.com>
2022-12-15 21:58:00 +0300

    Working on logic

--- src/components/TypingContainer/TextContainer/TextContainer.tsx
+++ src/components/TypingContainer/TextContainer/TextContainer.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
+const string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum";
+
+
 export default function TextContainer() {
+
+    const [text, setText] = useState<string[]>([]);
+
+    useEffect(() => {
+        (() => {
+            setText(string.split(""));
+        })();
+    }, []);
+
     return (
-        <Paper sx={{m: 3}} elevation={4}>
-            <Typography sx={{p: 2}} variant="h6">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                quasi quidem quibusdam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-                neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-                quasi quidem quibusdam.
-            </Typography>
+        <Paper sx={{m: 3, p: 2}} elevation={4}>
+            {
+                text.map((char, index) => (
+                    <Typography
+                        key={index}
+                        variant="h6"
+                        component="span"
+                    >
+                        {char}
+                    </Typography>
+                ))
+            }
         </Paper>
     );
 }
