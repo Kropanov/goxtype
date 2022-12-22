13f8ac4053119c5a98a467eb24ebec712c81deca
Maxim Kropanov <kropanov2012@gmail.com>
2022-12-22 17:41:04 +0300

    Changing design cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -12,29 +12,31 @@ export default function Cards() {
             <Grid container spacing={4}>
                 {CHALLENGES.map((challenge) => (
                     <Grid item key={challenge.id} xs={12} sm={6} md={4}>
-                        <Card
-                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                        >
-                            <Paper elevation={4}>
-                                <CardMedia
-                                    component="img"
-                                    image="https://source.unsplash.com/random"
-                                    alt="random"
-                                />
-                                <CardContent sx={{ flexGrow: 1 }}>
-                                    <Typography gutterBottom color={challenge.color} variant="h5" component="h2">
-                                        {challenge.complexity}
-                                    </Typography>
-                                    <Typography>
-                                        This is a media card. You can use this section to describe the
-                                        content.
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button variant="outlined" size="small">Solve Challenge</Button>
-                                </CardActions>
-                            </Paper>
-                        </Card>
+                        <Paper elevation={4}>
+                            <Card
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                            >
+                                <Paper elevation={4}>
+                                    <CardMedia
+                                        component="img"
+                                        image="https://source.unsplash.com/random"
+                                        alt="random"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom color={challenge.color} variant="h5" component="h2">
+                                            {challenge.complexity}
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button variant="outlined" size="small">Solve Challenge</Button>
+                                    </CardActions>
+                                </Paper>
+                            </Card>
+                        </Paper>
                     </Grid>
                 ))}
             </Grid>
