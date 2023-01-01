e2067a566079cfd7b0e7d43eff3d3acd33366510
Maxim Kropanov <kropanov2012@gmail.com>
2023-01-01 22:47:38 +0300

    Finish prototype of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,15 +10,40 @@ import {
 } from "@mui/material";
 import {SITE_NAME} from "../../constants/Constants";
 import Logo from "../Common/Logo/Logo";
+import SocialIcons from "../Common/SocialIcons/SocialIcons";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export function Footer() {
+
+    const SocialIconsData = [
+        {
+            icon: <FacebookIcon fontSize="large"/>,
+            label: "Facebook"
+        },
+        {
+            icon: <GoogleIcon fontSize="large" />,
+            label: "Google"
+        },
+        {
+            icon: <YouTubeIcon fontSize="large"/>,
+            label: "YouTube"
+        },
+        {
+            icon: <GitHubIcon fontSize="large" />,
+            label: "GitHub"
+        },
+    ];
+
     return (
         <Box sx={{ mt: "auto"}}>
             <Paper elevation={4}>
-                <Grid container justifyContent="center">
+                <Grid sx={{ pt: 5, pb: 5}} container justifyContent="center">
                     <Grid
                         item
-                        xs={4}
+                        xs={3}
                     >
                         <Box
                             sx={{
@@ -72,15 +97,30 @@ export function Footer() {
                         <List>
                             <ListItemButton>ABOUT US</ListItemButton>
                             <ListItemButton>CONTACT US</ListItemButton>
-                                            <ListItemButton>AFFILIATES</ListItemButton>
+                            <ListItemButton>AFFILIATES</ListItemButton>
                             <ListItemButton>RESOURCES</ListItemButton>
                         </List>
                     </Grid>
                 </Grid>
                 <Divider />
-                <Typography>
-                    ©Copyright. All rights reserved.
-                </Typography>
+                <Grid sx={{ pt: 4, pb: 4}} container justifyContent="center">
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            width: '100%',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Box sx={{ pt: 1, pb: 1 }}>
+                            <SocialIcons data={SocialIconsData} />
+                        </Box>
+                        <Typography>
+                            ©Copyright. All rights reserved.
+                        </Typography>
+                    </Box>
+                </Grid>
             </Paper>
         </Box>
     );
