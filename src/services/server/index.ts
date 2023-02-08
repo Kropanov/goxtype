0f75d29aa6b59850599578200612c40fb92eeb4f
Maxim Kropanov <kropanov2012@gmail.com>
2023-02-08 19:09:57 +0300

    Connecting database

--- src/services/server/index.ts
+++ src/services/server/index.ts
@@ -1,32 +1,31 @@
-// const dotenv = require('dotenv').config({ path: "./config.env" });
-// const mongoose = require('mongoose');
-// const express = require('express');
-// import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config({ path: "./config.env" });
 import express, { Express, Request, Response } from 'express';
+import mongoose from "mongoose";
+
+const DATABASE = process.env.DATABASE || "";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "";
+const PORT = process.env.PORT || 3001;
 
 const app: Express = express();
 
-// console.log(process.env.DATABASE_PASSWORD);
+const DB = DATABASE.replace(
+    "<password>",
+    DATABASE_PASSWORD
+);
 
-
-// const DB = process.env.DATABASE.replace(
-//     "<password>",
-//     process.env.DATABASE_PASSWORD
-// );
-//
-// mongoose.set('strictQuery', true);
-// mongoose.connect(DB)
-//     .then(() => {
-//         console.log('Connected!');
-//     });
+mongoose.set('strictQuery', true);
+mongoose.connect(DB)
+    .then(() => {
+        console.log('Connected!');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.get("/user", (req: Request, res: Response) => {
     res.json({ message: "Hello from Express!" });
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen( PORT, () => {
     console.log( `Server listening on ${PORT}...` );
