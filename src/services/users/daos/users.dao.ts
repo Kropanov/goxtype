a6b72b4762d283df45c78961fefd42b824c23aef
Maxim Kropanov <kropanov2012@gmail.com>
2023-02-10 23:45:20 +0300

    Adding daos for users

---
+++ src/services/users/daos/users.dao.ts
@@ -0,0 +1,7 @@
+import {CreateUserDto} from "../dto/create.user.dto";
+import {PutUserDto} from "../dto/put.user.dto";
+import {PatchUserDto} from "../dto/patch.user.dto";
+
+class UsersDao {}
+
+export default new UsersDao();
