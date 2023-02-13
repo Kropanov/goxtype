a84fd93d9ad63f2ca0d657018419e08ebc81bf13
Maxim Kropanov <kropanov2012@gmail.com>
2023-02-13 23:08:04 +0300

    Create crud operations

--- src/services/users/daos/users.dao.ts
+++ src/services/users/daos/users.dao.ts
@@ -2,6 +2,35 @@ import {CreateUserDto} from "../dto/create.user.dto";
 import {PutUserDto} from "../dto/put.user.dto";
 import {PatchUserDto} from "../dto/patch.user.dto";
 
-class UsersDao {}
+class UsersDao {
+
+    async addUser(user: CreateUserDto) {
+        return;
+    }
+
+    async getUsers() {
+        return;
+    }
+
+    async getUserById(userId: string) {
+        return;
+    }
+
+    async putUserById(userId: string, user: PutUserDto) {
+        return;
+    }
+
+    async patchUserById(userId: string, user: PatchUserDto) {
+        return;
+    }
+
+    async removeUserById(userId: string) {
+        return;
+    }
+
+    async getUserByEmail(email: string) {
+        return;
+    }
+}
 
 export default new UsersDao();
