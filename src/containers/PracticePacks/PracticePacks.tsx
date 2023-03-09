301786dcad1d53778cf729e505662a4fd7bc9c3d
Kropanov Maksim <kropanov2012@gmail.com>
2023-03-09 16:42:01 +0300

    Just adding skeleton

--- src/containers/PracticePacks/PracticePacks.tsx
+++ src/containers/PracticePacks/PracticePacks.tsx
@@ -1,23 +1,34 @@
 import React, {useEffect, useState} from "react";
-import {Container} from "@mui/material";
+import {Container, Skeleton} from "@mui/material";
 import Pack from "../../components/Pack/Pack";
 import {PackType} from "../../types/Types";
 
 export default function PracticePacks() {
     // ToDo rename variables
     const [value, setValue] = useState<PackType[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             const response = await fetch("/practice-packs").then(res => res.json());
             setValue(response);
+            // ToDo: remove after testing
+            setTimeout(() => {
+                setLoading(false);
+            }, 2000);
         })();
     }, []);
 
     return (
         <Container maxWidth="md">
             {
-                value.map((item, index) => (
+                loading ?
+                    <>
+                        {/*// ToDo: fix that ! :( */}
+                        <Skeleton sx={{m: 2}} animation="wave" variant="rounded" height={400} />
+                        <Skeleton sx={{m: 2}} animation="wave" variant="rounded" height={400} />
+                    </>
+                : value.map((item, index) => (
                     <Pack
                         key={index}
                         name={item.name}
