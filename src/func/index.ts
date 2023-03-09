8616cdd2fcd3008e58b2f832bfd15e4256bd322e
Kropanov Maksim <kropanov2012@gmail.com>
2023-03-09 15:00:28 +0300

    Working on packs

--- src/func/index.ts
+++ src/func/index.ts
@@ -1,3 +1,8 @@
 export function scrollPageUp() {
     window.scrollTo(0, 0);
 }
+
+export function toDefaultDate(date: string): string {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
+}
