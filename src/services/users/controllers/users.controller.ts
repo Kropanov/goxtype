2807e8ebbb1891d70d5c846777c0ccecda575511
Maxim Kropanov <kropanov2012@gmail.com>
2023-03-12 16:55:47 +0300

    Add argon2 to services

--- src/services/users/controllers/users.controller.ts
+++ src/services/users/controllers/users.controller.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import argon2 from 'argon2';
 import UsersService from "../service/users.service.js";
 
 class UsersController {
@@ -13,21 +14,21 @@ class UsersController {
     }
 
     async createUser(req: express.Request, res: express.Response) {
-        // req.body.password = await argon2.hash(req.body.password);
+        req.body.password = await argon2.hash(req.body.password);
         const userId = await UsersService.create(req.body);
         res.status(201).send({ id: userId });
     }
 
     async patch(req: express.Request, res: express.Response) {
-        // if (req.body.password) {
-        //     req.body.password = await argon2.hash(req.body.password);
-        // }
+        if (req.body.password) {
+            req.body.password = await argon2.hash(req.body.password);
+        }
         await UsersService.patchById(req.body.id, req.body);
         res.status(204).send();
     }
 
     async put(req: express.Request, res: express.Response) {
-        // req.body.password = await argon2.hash(req.body.password);
+        req.body.password = await argon2.hash(req.body.password);
         await UsersService.putById(req.body.id, req.body);
         res.status(204).send();
     }
