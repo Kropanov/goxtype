c311d5733d175c36a9855ec7353fe6e622170fad
Kropanov Maksim <kropanov2012@gmail.com>
2023-03-15 14:54:13 +0300

    Add method for protect routes

--- src/services/auth/middlewares/auth.middlewares.ts
+++ src/services/auth/middlewares/auth.middlewares.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import argon2 from "argon2";
 import UsersService from "../../users/service/users.service.js";
 
+// ToDo rename AuthMiddlewares
 class AuthMiddlewares {
     async verifyUserPassword(
         req: express.Request,
@@ -27,6 +28,25 @@ class AuthMiddlewares {
             status: "fail",
             message: "Invalid email and/or password!"
         });
+    }
+
+    // TODO: add this method to user routes
+    async protectRoutes(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+        if (!token || !req.headers.authorization?.startsWith("Bearer")){
+            return res.status(401).send({
+                status: "fail",
+                message: "Please login to get access!"
+            });
+        }
+        // TODO: don't forget, continue work
+
+        return next();
     }
 }
 
