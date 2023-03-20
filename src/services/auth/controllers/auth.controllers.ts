9914c0188fbb7bd74b7874c40bdb2a0b885cea4b
Kropanov Maksim <kropanov2012@gmail.com>
2023-03-20 08:57:08 +0300

    Refactor code

--- src/services/auth/controllers/auth.controllers.ts
+++ src/services/auth/controllers/auth.controllers.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import argon2 from "argon2";
-import UsersService from "../../users/service/users.service.js";
+import UserService from "../../users/service/users.service.js";
 import JWTService from "../../common/service/jwt.service.js";
 
 class AuthController {
@@ -20,7 +20,7 @@ class AuthController {
 
     async signup(req: express.Request, res: express.Response) {
         req.body.password = await argon2.hash(req.body.password);
-        const user = await UsersService.create(req.body);
+        const user = await UserService.create(req.body);
         const token = JWTService.createJWT(user._id);
         res.status(201).send({
             status: "success",
