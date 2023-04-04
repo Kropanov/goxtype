8ca9f65587daa6fae9b6ab6403395ee0f88bead6
Kropanov Maksim <kropanov2012@gmail.com>
2023-04-04 13:54:02 +0300

    Fix

--- src/components/QRCode/Mobile/MobileQRCode.tsx
+++ src/components/QRCode/Mobile/MobileQRCode.tsx
@@ -8,29 +8,18 @@ export default function MobileQRCode() {
         <Container sx={{mb: 4}} maxWidth="md">
             <Paper elevation={4}>
                 <Grid sx={{ p: 2 }} container>
-                    <Grid item md={8} xs={12}>
+                    <Grid item md={12} xs={12}>
                         <Box sx={{
                             display: 'flex',
                             width: '100%',
                             height: '100%',
                             alignItems: 'center',
-                            justifyContent: 'center',
+                            justifyContent: 'space-evenly'
                         }}>
-                            <InstallMobileIcon />
+                            <InstallMobileIcon fontSize="large" />
                             <Typography variant="h6">
                                 Install our mobile app for additional practice!
                             </Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item md={4} xs={12}>
-                        <Box sx={{
-                            display: 'flex',
-                            width: '100%',
-                            height: '100%',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            p: 1
-                        }}>
                             <QRCodeSVG value="https://reactjs.org/" size={100} />
                         </Box>
                     </Grid>
