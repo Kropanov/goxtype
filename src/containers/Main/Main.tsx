0341c232c50169dbcbe2fd5fb079fa6f390c5339
sw1pr0g <80714499+sw1pr0g@users.noreply.github.com>
2023-11-20 14:44:57 +0300

    Remove QRCode and qrcode package (#71)
    * Remove QRCode and qrcode package
    
    * Fix module not found problem
    
    ---------
    
    Co-authored-by: Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Cards from "../../components/Cards/Cards";
 import TypingTest from "../../components/TypingTest/TypingTest";
-import MobileQRCode from "../../components/QRCode/Mobile/MobileQRCode";
 
 export default function Main() {
     return (
         <>
             <TypingTest />
-            <MobileQRCode />
             <Cards />
         </>
     );
